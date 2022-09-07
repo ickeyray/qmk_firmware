b4a3939ce370d50ae0018ef2eabef55ec10f3043
Ickeyray <>
2022-09-07 20:18:22 +1000

    rotated secondary oled

--- keyboards/sofle/keymaps/rix_rgb/keymap.c
+++ keyboards/sofle/keymaps/rix_rgb/keymap.c
@@ -629,10 +629,13 @@ static void print_status_narrow(void) {
 }
 
 oled_rotation_t oled_init_user(oled_rotation_t rotation) {
-    if (is_keyboard_master()) {
-        return OLED_ROTATION_270;
-    }
-    return rotation;
+
+    return OLED_ROTATION_270;
+
+    // if (is_keyboard_master()) {
+    //     return OLED_ROTATION_270;
+    // }
+    // return rotation;
 }
 
 bool oled_task_user(void) {
