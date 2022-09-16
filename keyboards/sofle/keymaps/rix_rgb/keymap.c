2998aecf6c5acc9b96de75be4d645ba2283b1cd1
Ickeyray <>
2022-09-16 16:17:42 +1000

    oled bug fix?

--- keyboards/sofle/keymaps/rix_rgb/keymap.c
+++ keyboards/sofle/keymaps/rix_rgb/keymap.c
@@ -533,10 +533,10 @@ static void render_luna(int LUNA_X, int LUNA_Y) {
     }
 
     /* this fixes the screen on and off bug */
-    // if (current_wpm > 0) {
-    //     oled_on();
-    //     anim_sleep = timer_read32();
-    // } 
+    if (current_wpm > 0) {
+        oled_on();
+        anim_sleep = timer_read32();
+    } 
     // else if (timer_elapsed32(anim_sleep) > OLED_TIMEOUT) {
     //     oled_off();
     // }
