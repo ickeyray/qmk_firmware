dd1c672c1a7565c99c0d9c5f8c8e41e9c1f11dde
Ickeyray <>
2022-10-26 15:44:43 +1100

    change left dial to RGB tog

--- keyboards/sofle/keymaps/rix_rgb/keymap.c
+++ keyboards/sofle/keymaps/rix_rgb/keymap.c
@@ -109,7 +109,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //|------+-------+--------+--------+--------+------|                   |--------+-------+--------+--------+--------+---------|
   KC_TAB,   KC_A,KC_S, KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,   KC_K,    KC_L,    KC_SCLN, KC_QUOT,
   //|------+-------+--------+--------+--------+------|  ===  |   |  ===  |--------+-------+--------+--------+--------+---------|
-  KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B,  KC_CAPS,  KC_MUTE,  KC_N,    KC_M,   KC_COMM, KC_DOT,  KC_SLSH, KC_LSFT,
+  KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B,  RGB_TOG,  KC_MUTE,  KC_N,    KC_M,   KC_COMM, KC_DOT,  KC_SLSH, KC_LSFT,
   //|------+-------+--------+--------+--------+------|  ===  |   |  ===  |--------+-------+--------+--------+--------+---------|
                  KC_LALT, KC_LGUI,  KC_LCTRL, KC_ENT, KC_LOWER, KC_RAISE, KC_SPC, KC_RCTRL, KC_RGUI, KC_RALT
   //            \--------+--------+--------+---------+-------|   |--------+---------+--------+---------+-------/
